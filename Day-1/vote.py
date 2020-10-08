f0deb0b4b2c9f90aeb5984268c265a46c17cf921
Anurag-Code-Hub <72536128+Anurag-Code-Hub@users.noreply.github.com>
2020-10-08 16:42:42 +0530

    updated

---
+++ Day-1/vote.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+print(f"Hello, {name}")
+
+name=input("enter name\n")
+age=input("enter age\n")
+print(f"hello {name}")
+if int(age) > 18:
+    print(f"{name} is eligible for vote")
+else:
+    print(f"{name} is not eligible for vote")
